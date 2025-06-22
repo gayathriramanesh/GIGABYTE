0140155d250593ba5c462be0063a059d929e36a0
gayathriramanesh <gayathriramanesh2001@gmail.com>
2025-06-22 23:00:20 +0530

    FInal check

--- backend/checkout.py
+++ backend/checkout.py
@@ -28,50 +28,56 @@ class CheckoutRequest(BaseModel):
 def place_order(
     payload: CheckoutRequest,
     current_user: Annotated[Register, Depends(get_current_user)],db: Session = Depends(get_db)):
-    user_id = current_user.id
-    cart_items = get_cart(user_id)
-    if not cart_items:
-        raise HTTPException(status_code=400, detail="Cart is empty")
+    try:
+        print("Payload:", payload)
+        print("User ID:", current_user.id)
+        user_id = current_user.id
+        cart_items = get_cart(user_id)
+        if not cart_items:
+            raise HTTPException(status_code=400, detail="Cart is empty")
 
-    total_price = 0
-    order_items = []
+        total_price = 0
+        order_items = []
 
-    for item in cart_items:
-        product = db.query(Product).filter(Product.pid==item["product_id"]).first()
-        if not product:
-            print(product)
-            raise HTTPException(status_code=404, detail=f"Product {item['product_id']} not found")
+        for item in cart_items:
+            product = db.query(Product).filter(Product.pid==item["product_id"]).first()
+            if not product:
+                print(product)
+                raise HTTPException(status_code=404, detail=f"Product {item['product_id']} not found")
 
-        item_total = float(product.price) * item["quantity"]
-        total_price += item_total
+            item_total = float(product.price) * item["quantity"]
+            total_price += item_total
 
-        order_items.append(OrderItem(
-            product_id=product.pid,
-            quantity=item["quantity"],
-            price=product.price
-        ))
+            order_items.append(OrderItem(
+                product_id=product.pid,
+                quantity=item["quantity"],
+                price=product.price
+            ))
 
-    order = Order(
-        user_id=user_id,
-        address=payload.address,
-        pincode=payload.pincode,
-        phone=payload.phone,
-        payment_method=payload.payment_method,
-        total_price=total_price,
-        payment_status=True,  
-        order_date=datetime.now(),
-        items=order_items
-    )
+        order = Order(
+            user_id=user_id,
+            address=payload.address,
+            pincode=payload.pincode,
+            phone=payload.phone,
+            payment_method=payload.payment_method,
+            total_price=total_price,
+            payment_status=True,  
+            order_date=datetime.now(),
+            items=order_items
+        )
 
-    db.add(order)
-    db.commit()
-    db.refresh(order)
+        db.add(order)
+        db.commit()
+        db.refresh(order)
 
-    clear_cart(user_id)
+        clear_cart(user_id)
 
-    return {
-        "message": "Order placed successfully",
-        "order_id": order.order_id,
-        "total": total_price,
-        "items": [{"product_id": i.product_id, "qty": i.quantity} for i in order.items]
-    }
+        return {
+            "message": "Order placed successfully",
+            "order_id": order.order_id,
+            "total": total_price,
+            "items": [{"product_id": i.product_id, "qty": i.quantity} for i in order.items]
+        }
+    except Exception as e:
+        print("Checkout Error:", e)
+        raise HTTPException(status_code=500, detail="Internal Server Error")
